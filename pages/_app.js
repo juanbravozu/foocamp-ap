6c77bca100c964b9be50150748e207026ed9cf5a
jcamacho <jcamacho@hugeinc.com>
2021-07-27 13:21:04 -0500

    Initial commit from Create Next App

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/_variables.scss';
+import '../styles/globals.scss';
+import Layout from '../components/Layout';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
