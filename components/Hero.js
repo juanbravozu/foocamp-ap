ff5112958847041cf6ec311e360f80e7dcd9f4fe
Juan Bravo <juanda.bravo98@gmail.com>
2021-08-18 02:39:50 -0500

    Hero component done

--- components/Hero.js
+++ components/Hero.js
@@ -4,21 +4,34 @@ export default function Hero({
   title, description, bgImg, ctaText, ctaLink, socialMedia,
 }) {
   return (
-    <section>
-      <img src={bgImg} alt="" />
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <Link href={`/${ctaLink}`}>
-        <p>{ctaText}</p>
-      </Link>
+    <section className="hero">
+      <img src={bgImg} alt="" className="hero-background" />
 
-      {socialMedia.map((item) => (
-        <img
-          key={item.fields.title}
-          src={item.fields.section.fields.field.url}
-          alt={item.fields.title}
-        />
-      ))}
+      <div className="hero-shape" />
+
+      <div className="hero-content">
+        <h1 className="hero-title">{title}</h1>
+        <p className="hero-description">{description}</p>
+        <Link href={`/${ctaLink}`}>
+          <p className="button-main hero-button">{ctaText}</p>
+        </Link>
+
+        <div className="hero-media-wrapper">
+          <div className="hero-line" />
+          {socialMedia.map((item) => (
+            <a href={item.fields.link} target="_blank" rel="noreferrer">
+              <img
+                key={item.fields.title}
+                src={item.fields.icon.fields.file.url}
+                alt={item.fields.title}
+                className="hero-media"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <img src="/icons/arrow-down.svg" className="hero-arrow" alt="" />
     </section>
   );
 }
