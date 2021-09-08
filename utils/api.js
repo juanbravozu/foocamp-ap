970fdddd4b7ded081f42bc241f730a05cba6fda2
Juan Bravo <juanda.bravo98@gmail.com>
2021-09-07 21:58:34 -0500

    Changed method to retrieve information from contentful

--- utils/api.js
+++ utils/api.js
@@ -6,11 +6,14 @@ const client = createClient({
 });
 
 export default async function getPageData(pageSlug) {
-  const response = await client.getEntries({ content_type: 'page' });
+  const baseUrl = `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/entries`;
+  const query = `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=page`;
+  const pageFilter = `&fields.slug=${pageSlug}`;
 
-  const targetPage = response.items.find((page) => page.fields.slug === pageSlug);
+  const response = await fetch(`${baseUrl}/${query}${pageFilter}`);
+  const { items } = await response.json();
 
-  const pageData = await client.getEntry(targetPage.sys.id, { include: 3 });
+  const pageData = await client.getEntry(items[0].sys.id, { include: 3 });
 
   return pageData;
 }
