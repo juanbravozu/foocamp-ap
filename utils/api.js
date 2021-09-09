f9d9c573d30608128558bdda5ef0f5b67235e36b
Juan Bravo <juanda.bravo98@gmail.com>
2021-09-08 20:37:03 -0500

    Added base url constant and try catch block to api call

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful';
+import { BASE_URL } from './constants';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE,
@@ -6,14 +7,18 @@ const client = createClient({
 });
 
 export default async function getPageData(pageSlug) {
-  const baseUrl = `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/entries`;
-  const query = `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=page`;
-  const pageFilter = `&fields.slug=${pageSlug}`;
+  try {
+    const spaceUrl = `${BASE_URL}/${process.env.CONTENTFUL_SPACE}/entries`;
+    const query = `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=page`;
+    const pageFilter = `&fields.slug=${pageSlug}`;
 
-  const response = await fetch(`${baseUrl}/${query}${pageFilter}`);
-  const { items } = await response.json();
+    const response = await fetch(`${spaceUrl}/${query}${pageFilter}`);
+    const { items } = await response.json();
 
-  const pageData = await client.getEntry(items[0].sys.id, { include: 3 });
+    const pageData = await client.getEntry(items[0].sys.id, { include: 3 });
 
-  return pageData;
+    return pageData;
+  } catch (e) {
+    return console.error(e);
+  }
 }
