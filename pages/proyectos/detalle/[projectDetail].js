d1086945e1533d30c3f2c5e43414aa2b7e20ed48
Juan Bravo <juanda.bravo98@gmail.com>
2021-09-16 17:07:31 -0500

    Fetch method for project details

--- pages/proyectos/detalle/[projectDetail].js
+++ pages/proyectos/detalle/[projectDetail].js
@@ -1,4 +1,25 @@
-export default function ProjectDetail() {
+import getPageData from '../../../utils/api';
+
+export const getServerSideProps = async (context) => {
+  try {
+    const { projectDetail } = context.query;
+    const pageData = await getPageData(projectDetail, 'project');
+
+    return {
+      props: {
+        data: pageData,
+        components: pageData.fields.projectDetail.fields,
+      },
+    };
+  } catch (e) {
+    return console.error(e);
+  }
+};
+
+/* eslint-disable no-unused-vars */
+export default function ProjectDetail({ components }) {
+  /* eslint-enable no-unused-vars */
+
   return (
     <div>
       <h1>Detail 1</h1>
