2e3e32ab2aea4d3f56e9c49e76ef0560394f0d1a
Juan Bravo <juanda.bravo98@gmail.com>
2021-09-16 13:13:36 -0500

    Adapted hero to projects page

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,21 @@
 import Link from 'next/link';
 
-export default function Hero({ fields }) {
+export default function Hero({
+  fields,
+  baseUrl = '',
+  linksToBlank = true,
+  hideLine = false,
+}) {
   const {
     title: { fields: { title: sectionTitle } },
     description,
     heroBackground: { fields: { file: { url: img } } },
-    cta: { fields: { link: ctaLink, label: ctaLabel } },
-    socialNetworksSection,
+    cta: {
+      fields: {
+        link: ctaLink = null, label: ctaLabel = null,
+      } = {},
+    } = {},
+    socialNetworksSection = [],
   } = fields;
 
   return (
@@ -18,26 +27,51 @@ export default function Hero({ fields }) {
       <div className="hero-content">
         <h1 className="hero-title">{sectionTitle}</h1>
         <p className="hero-description">{description}</p>
+        {!!ctaLink && (
         <Link href={`/${ctaLink}`}>
           <p className="button-main hero-button">{ctaLabel}</p>
         </Link>
+        )}
 
-        <div className="hero-media-wrapper">
-          <div className="hero-line" />
-          {socialNetworksSection.map(({
-            fields: { title, icon, link },
-            sys: { id },
-          }) => (
-            <a href={link} target="_blank" rel="noreferrer" key={id}>
-              <img
-                key={title}
-                src={icon.fields.file.url}
-                alt={title}
-                className="hero-media"
-              />
-            </a>
-          ))}
-        </div>
+        {socialNetworksSection.length > 0
+          && (
+          <div className="hero-media-wrapper">
+            {!hideLine && <div className="hero-line" />}
+            {socialNetworksSection.map(({
+              fields: { title, icon, link },
+              sys: { id },
+            }) => (
+              linksToBlank
+                ? (
+                  <a href={`${baseUrl}${link}`} target="_blank" rel="noreferrer" key={id}>
+                    <img
+                      key={title}
+                      src={icon.fields.file.url}
+                      alt={title}
+                      className="hero-media"
+                    />
+                  </a>
+                )
+                : (
+                  <Link
+                    href={`${baseUrl}${link}`}
+                    passHref
+                    key={id}
+                    shallow
+                  >
+                    <a href="/">
+                      <img
+                        key={title}
+                        src={icon.fields.file.url}
+                        alt={title}
+                        className="hero-media"
+                      />
+                    </a>
+                  </Link>
+                )
+            ))}
+          </div>
+          )}
       </div>
 
       <img src="/icons/arrow-down.svg" className="hero-arrow" alt="" />
